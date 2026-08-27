a00ddeb8373997aac6f2d0d1a370ad6e3597e1d2
IVAN.KORCHAHIN <ikorcagin11@gmail.com>
2026-08-27 18:16:22 +0200

    task_manager

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,16 +1,40 @@
 from django.contrib import admin
 from .models import Task, SubTask, Category
 
+
+#
+
+#
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
+
+
+class Taskinline(admin.StackedInline):
+    model = SubTask
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title','description','deadline','status')
+    list_display = ('short_title','description','deadline','status')
+    inlines = [Taskinline]
+
+    @admin.display(description='название')
+    def short_title(self, obj):
+        if len(obj.title)>10:
+            return obj.title[:10] + "..."
+        return obj.title
+
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title','description','deadline','status')
+    actions = ['make_done']
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    @admin.action(description='готово')
+    def make_done(self, request, queryset):
+        queryset.update(status="done")
+
+
 
 
